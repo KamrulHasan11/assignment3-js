e6e86b35e2941472287907933202db3da2b05765
Kamrul Hasan <kamrulhasanit5@gmail.com>
2021-01-22 20:56:09 +0600

    added js file

---
+++ assignment.js
@@ -0,0 +1,26 @@
+// Start kilometerToMeter function
+function kilometerToMeter(){
+    var km = 
+}
+// End of kilometerToMeter function
+
+
+// Start budgetCalculator function
+function budgetCalculator(){
+
+}
+// End of budgerCalculator function
+
+
+// Start of hotelCost function
+function hotelCost(){
+
+}
+// End of hotelCost function
+
+
+// Start of megaFriend function
+function megaFriend(){
+
+}
+// End of megaFriend function
